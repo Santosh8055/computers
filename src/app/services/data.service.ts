bd66a46a9cf9776a5ee8f7cf966d990ff7926a40
Santosh8055 <santosh.pasupunuri@gmail.com>
2018-07-05 00:20:23 +0530

    post request addition

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,15 +1,24 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-
-import { Observable, of } from 'rxjs';
-
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+// import { Observable, of } from 'rxjs';
+const httpOptions = {
+	headers: new HttpHeaders({
+		'Content-Type': 'application/json'
+	})
+};
 @Injectable({
 	providedIn: 'root'
 })
 export class DataService {
-	private configUrl = 'https://api.myjson.com/bins/1e04hg';
+	private getComputerUrl = 'https://api.myjson.com/bins/1e04hg';
+	private addComputerUrl = 'https://jsonplaceholder.typicode.com/posts';
+
 	constructor(private http: HttpClient) { }
 	getComputers() {
-		return this.http.get(this.configUrl);
+		return this.http.get(this.getComputerUrl);
+	}
+
+	addComputers(computer) {
+		return this.http.post<any>(this.addComputerUrl, computer, httpOptions);
 	}
 }
